3ee4687b39c84e60d98bbfbbd968659ae685497d
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2023-05-26 16:20:20 -0700

    made login page

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LandingPage } from "./pages/LandingPage";
+import { LoginPage } from "./pages/LoginPage";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={LandingPage} />
+    {/* <Stack.Screen name="Landing Page" component={LandingPage} /> */}
+    <Stack.Screen name="Login Page" component={LoginPage} />
     </Stack.Navigator>
   </NavigationContainer>
   );
--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -17,7 +17,7 @@ export const LandingPage = () => {
         source={require("../assets/Dot.png")}
       >
         <View style={styles.div}>
-          <Image source={require("../journalIcon.png")} style={styles.image} />
+          <Image source={require("../assets/journalIcon.png")} style={styles.image} />
         </View>
 
         <Text style={styles.text1}>Plannify</Text>
